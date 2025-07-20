a0e3992357cce32b6b57a34a13f317157d39b684
Avinash HM <avi.hm24@gmail.com>
2025-07-20 20:26:54 +0000

    I have added all keys in .env now everything should work fine ,try and c

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({projectId: process.env.GOOGLE_PROJECT_ID})],
   model: 'googleai/gemini-2.0-flash',
 });
